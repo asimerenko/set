8f62160c74eef4e3d0d75a2236f1258e0aad27d2
asimerenko <asimerenko@sfedu.ru>
2025-03-30 17:51:53 +0300

    Create main.py
    find the intersection of sets and output the list

---
+++ main.py
@@ -0,0 +1,9 @@
+def get_intersection(set1, set2):
+    set1.add(60) 
+    return set1.intersection(set2)
+
+if __name__ == "__main__":
+    int_set_1 = {23, 45, 12, 7}
+    int_set_2 = {70, 60, 12, 6, 9}
+    int_set_3 = get_intersection(int_set_1, int_set_2)
+    print(list(int_set_3))
