76cb36cb9893c4a5faed2a0cffdb8c2a66acebfa
asimerenko <asimerenko@sfedu.ru>
2025-03-30 18:27:33 +0300

    Update main.py with errors

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def get_intersection(set1, set2):
 if __name__ == "__main__":
     int_set_1 = {23, 45, 12, 7}
     int_set_2 = {70, 60, 12, 6, 9}
-    int_set_3 = get_intersection(int_set_1, int_set_2)
+    int_set_3 = get_intersection(int_set_2, int_set_2)
     print(list(int_set_3))
